19639e26271289bd40663d46117461bcdd2112d2
maars202 <simonslee332@gmail.com>
2023-02-04 20:27:13 +0800

    duty_hour_log working table added

---
+++ backend/Duty_Hour_Log.py
@@ -0,0 +1,29 @@
+
+from __main__ import app,db
+from flask import request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+class Duty_Hour_Log(db.Model):
+    __tablename__ = 'Duty_Hour_Log'
+    Employee_id = db.Column(db.String(100), primary_key=True)
+    Level = db.Column(db.String(100))
+    Submitted = db.Column(db.String(100))
+    Submitted_Proportion = db.Column(db.String(100))
+    MMYYYY = db.Column(db.String(100))
+    Logged_for_month = db.Column(db.String(100))
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'Involvement'
+    }
+
+    def to_dict(self):
+        """
+        'to_dict' converts the object into a dictionary,
+        in which the keys correspond to database columns
+        """
+        columns = self.__mapper__.column_attrs.keys()
+        print(f"columns: {columns}")
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
